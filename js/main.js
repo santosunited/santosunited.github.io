97059641b7fe803835f75512348d295872f1e512
ischools <ischoolsdesign@gmail.com>
2015-03-25 18:59:47 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/*
+$(document).ready(function() {
+    $("#timbers_army").mouseover(function() {
+        $("#timber_army-info").fadeIn();
+        $("#timbers_army").css("opacity", "1");
+    });
+    $("#family_sponsors").mouseout(function() {
+       $("#timber_army-info").fadeOut();
+        $("#timbers_army").css("opacity", "0.5");
+    });
+    $("#clp").mouseover(function() {
+        $("#clp-info").fadeIn();
+        $("#clp").css("opacity", "1");
+    });
+    $("#clp").mouseout(function() {
+       $("#clp-info").fadeOut();
+        $("#clp").css("opacity", "0.5");
+    });
+    $("#timbers").mouseover(function() {
+        $("#timbers-info").fadeIn();
+        $("#timbers").css("opacity", "1");
+    });
+    $("#timbers").mouseout(function() {
+       $("#timbers-info").fadeOut();
+        $("#timbers").css("opacity", "0.5");
+    });
+    $("#xtreme").mouseover(function() {
+        $("#xtreme-info").fadeIn();
+        $("#xtreme").css("opacity", "1");
+    });
+    $("#xtreme").mouseout(function() {
+       $("#xtreme-info").fadeOut();
+        $("#xtreme").css("opacity", "0.5");
+    });
+});
+*/
